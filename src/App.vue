3ce2662b98d1bf554ac749fbf92123a2b5551044
Phan Hong Nam (TrippleN) <phan.hong.nam@azoom.jp>
2022-07-11 14:53:07 +0700

    :sparkles: Add some reactive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface AppState {
+  name: string;
+  rawHtml: string;
+}
+
+const state = reactive<AppState>({
+  name: "PHNam (TrippleN)",
+  rawHtml: "<span style='color: red'>Xiao</span>",
+});
+
+const anotherState = shallowReactive({
+  foo: 1,
+  nested: {
+    bar: 2,
+  },
+});
+
+const count = ref<number>(0);
+
+const increaseCount = () => {
+  count.value++;
+};
+
+const doubleCount = computed<number>(() => count.value * 2);
+
+watch([count, doubleCount], ([newCount, newDoubleCount]): void => {
+  console.log(`Count: ${newCount}, doubleCount: ${newDoubleCount}`);
+});
+
+anotherState.nested.bar++;
+</script>
 
 <template>
-  <div>Hello World!</div>
+  <div>
+    <p>Hello {{ state.name }}!</p>
+    <p v-html="state.rawHtml"></p>
+    <!-- Ref auto unwrapping in template, no need to use .value -->
+    <p>Count: {{ count }}</p>
+    <p>Double count: {{ doubleCount }}</p>
+    <button @click="increaseCount">Increase</button>
+    <p>{{ anotherState.nested.bar }}</p>
+  </div>
 </template>
 
 <style>
