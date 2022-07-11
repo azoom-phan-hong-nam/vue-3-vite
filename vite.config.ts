77f388c51289929f38bb7b02212689d63984a220
Phan Hong Nam (TrippleN) <phan.hong.nam@azoom.jp>
2022-07-11 09:36:56 +0700

    :heavy_plus_sign: Add auto import vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        "vue",
+      ],
+      dts: true,
+    }),
+  ],
+});
